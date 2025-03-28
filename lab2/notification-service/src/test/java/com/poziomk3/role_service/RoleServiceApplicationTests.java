ef023a9ddad15da9dfc30bd1e021e3c3d5237d85
Karol Zając <karolzajac.0407@gmail.com>
2025-03-28 17:14:58 +0100

    feat: copy service

---
+++ lab2/notification-service/src/test/java/com/poziomk3/role_service/RoleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poziomk3.role_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ lab2/user-man-service/src/main/java/com/poziomk3/role_service/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.poziomk3.role_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.InputStream;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPIFromYaml() {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("openapi.yaml");
+        SwaggerParseResult parseResult = new OpenAPIV3Parser().readLocation("src/main/resources/openapi.yaml", null, null);
+        return parseResult.getOpenAPI();
+    }
+}
